0dabcf390f8907381a3f89d6af2bcd60a1d112d1
Fikkie007 <fikkieakbar48@gmail.com>
2024-02-06 22:03:05 +0700

    Update dependencies and devDependencies

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const monsterrat = Montserrat({
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${monsterrat.className} bg-[#E1EAF3]`}>{children}</body>
     </html>
   );
 }
