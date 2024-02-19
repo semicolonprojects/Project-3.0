250cfe455f118aa51e07799bff6caee597410564
Fikkie007 <fikkieakbar48@gmail.com>
2024-02-19 18:23:23 +0700

    Changed the import path of the image asset `WhiteShoes` from `public/img/sepatu_putih(home).jpg` to `../../public/img/sepatu_putih(home).jpg`.

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import WhiteShoes from "@/public/img/sepatu_putih(home).jpg";
+import WhiteShoes from "../../public/img/sepatu_putih(home).jpg";
 
 const Page = () => {
   return <Image className="h-[728px]" src={WhiteShoes} alt="...." />;
