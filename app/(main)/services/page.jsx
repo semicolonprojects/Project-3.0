77920569964f5a8494781237aea39070b7cf04a3
Breyhan-F9 <breyhan20@gmail.com>
2024-03-12 21:41:02 +0700

    fixing services page layout and responsive

--- app/(main)/services/page.jsx
+++ app/(main)/services/page.jsx
@@ -25,15 +25,15 @@ function Page() {
 
   return (
     <>
-      <div className="py-16 tablet:py-5 w-full">
+      <div className="py-16 tablet:py-5 w-full ">
         {/*  Cards Service 1 */}
-        <div className="py-2 grid grid-rows-1 gap-x-20 gap-y-10 tablet:grid-cols-4 laptop:grid-cols-4 desktop:grid-cols-4 justify-center tablet:justify-start items-center tablet:items-start">
+        <div className="py-2 grid grid-rows-1 gap-x-7 gap-y-10 tablet:grid-flow-col justify-center tablet:justify-start items-center tablet:items-start">
           <Link href="/services/deep-clean" className="group">
-            <div className="aspect-h-1 aspect-w-1 w-[289px] h-[389px] overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
+            <div className="aspect-h-1 aspect-w-1 w-[250px] h-[389px] overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
               <Image
                 src="/img/1.png"
                 alt="..."
-                width="289"
+                width="200"
                 height="389"
                 className="h-full w-full object-cover  group-hover:opacity-75"
               />
@@ -43,11 +43,11 @@ function Page() {
             </h3>
           </Link>
           <Link href="/services/reglue" className="group">
-            <div className="aspect-h-1 aspect-w-1  w-[289px] h-[389px]  overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
+            <div className="aspect-h-1 aspect-w-1  w-[250px] h-[389px]  overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
               <Image
                 src="/img/2.png"
                 alt="..."
-                width="289"
+                width="200"
                 height="389"
                 className="h-full w-full object-cover  group-hover:opacity-75"
               />
@@ -57,11 +57,11 @@ function Page() {
             </h3>
           </Link>
           <Link href="" className="group">
-            <div className="aspect-h-1 aspect-w-1  w-[289px] h-[389px]  overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
+            <div className="aspect-h-1 aspect-w-1  w-[250px] h-[389px]  overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
               <Image
                 src="/img/3.png"
                 alt="..."
-                width="289"
+                width="200"
                 height="389"
                 className="h-full w-full object-cover  group-hover:opacity-75"
               />
@@ -71,11 +71,11 @@ function Page() {
             </h3>
           </Link>
           <Link href="" className="group">
-            <div className="aspect-h-1 aspect-w-1  w-[289px] h-[389px]  overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
+            <div className="aspect-h-1 aspect-w-1  w-[250px] h-[389px]  overflow-hidden  bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
               <Image
                 src="/img/3.png"
                 alt="..."
-                width="289"
+                width="200"
                 height="389"
                 className="h-full w-full object-cover  group-hover:opacity-75"
               />
